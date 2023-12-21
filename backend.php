251199b4fdf79053e4d097dd6d06b2fb015e9131
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2023-12-21 21:02:20 +0100

    changed to post

--- backend.php
+++ backend.php
@@ -3,17 +3,28 @@ header("Access-Control-Allow-Origin: *"); // Allows all origins
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 
-
-
 parse_str($_SERVER['QUERY_STRING'], $params);
 
-//wenn Name gesetzt wurde schicke 
-if (isset($params['name']) && $params['name'] != '') {
-    // produce result
-    $result['message'] = 'Backend funktioniert ' . $params['name'];
+// Check if it's a POST request
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    echo json_encode("post erkannt");
+    // Get the POST data
+    $postData = file_get_contents('php://input');
+    echo ($postData);
+    $postData = json_decode($postData, true);
+
+
+    // Check if the 'name' parameter is set in the POST data
+    if (isset($postData['name']) && $postData['name'] != '') {
+        // Produce result
+        $result['message'] = 'Backend funktioniert mit post' . $postData['name'];
+    } else {
+        $result['error'] = 'Parameter <name> is not set or empty';
+        http_response_code(400);
+    }
 } else {
-    $result['error'] = 'Parameter <name> is not set or empty';
-    http_response_code(400);
+    $result['error'] = 'Invalid request method';
+    http_response_code(405);
 }
 
 echo json_encode($result);
