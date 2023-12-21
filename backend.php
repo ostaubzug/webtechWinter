3f98dd9b08ff13676dcc1cc70ebb6899732c3c52
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2023-12-21 18:35:21 +0100

    working on backen

--- backend.php
+++ backend.php
@@ -2,21 +2,14 @@
 
 parse_str($_SERVER['QUERY_STRING'], $params);
 
-
-
+//wenn Name gesetzt wurde schicke 
 if (isset($params['name']) && $params['name'] != '') {
     // produce result
-    $result['message'] = 'Hello ' . $params['name'];
+    $result['message'] = 'Backend funktioniert ' . $params['name'];
 } else {
     $result['error'] = 'Parameter <name> is not set or empty';
     http_response_code(400);
 }
 
 echo json_encode($result);
-
-function myFunc($param1, $param2, $param3)
-{
-    return $param1;
-}
-
 ?>
