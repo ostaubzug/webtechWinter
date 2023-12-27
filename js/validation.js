9663d8d17f3d0b21c0d6797e76b9a63a276aa2bb
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2023-12-27 19:44:26 +0100

    Frontend Validierung

--- js/validation.js
+++ js/validation.js
@@ -1,3 +1,5 @@
+let date = document.getElementById("kaufdatum");
+let phonemodel = document.getElementById("phonemodel");
 let customerName = document.getElementById("name");
 let email = document.getElementById("email");
 
@@ -10,6 +12,8 @@ function isInputValid() {
   clearAllValidationErrors();
   validateEmail();
   validateName();
+  validatePhoneModel();
+  validateDate();
 
   if (!hasValidationErrors()) {
     return true;
@@ -24,12 +28,30 @@ function clearAllValidationErrors() {
   errorEmail.innerText = "";
 }
 
-function validatePhoneModel() {}
+function validateDate() {
+  var now = new Date();
+  if (date.validity.valueMissing) {
+    errorDate.innerText = "Bitte füllen Sie dieses Feld aus";
+  } else if (new Date(date.value) > now) {
+    errorDate.innerText = "Das Datum muss in der Vergangenheit liegen";
+  } else if (!date.checkValidity()) {
+    errorDate.innerText = "Bitte geben Sie ein gültiges Datum ein";
+  }
+}
+
+function validatePhoneModel() {
+  if (phonemodel.value == "" || phonemodel.value < 1 || phonemodel.value > 6) {
+    errorModel.innerText = "Bitte wählen Sie ein gültiges Modell aus";
+  }
+}
 
 function validateName() {
   if (customerName.value.length < 5 || customerName.value.length > 20) {
     errorName.innerText =
       "Geben Sie einen Namen mit der Länge 5 - 20 Zeichen ein";
+    //check if there are any numbers in the name with regex
+  } else if (/\d/.test(customerName.value)) {
+    errorName.innerText = "Der Name darf keine Zahlen enthalten";
   } else if (!customerName.checkValidity()) {
     errorName.innerText = "Bitte geben Sie einen gültigen Namen ein";
   }
